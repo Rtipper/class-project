c32e8842e59872f9d3af91b79fe157f63f0136ff
RYAN <RTIPPER44@GMAIL.COM>
2020-09-14 21:28:45 -0700

    updated site and added in class 7 function commands

--- js/ask-name.js
+++ js/ask-name.js
@@ -1,5 +1,11 @@
 'use strict'
 
-var userName = prompt('What is your name?');
+function askName() {
+    var userName = prompt('What is your name?');
+    return document.write('<h2>' + 'Great to meet you ' + userName + '<h2>');
+}
 
-document.write('<h2>' + 'Great to meet you ' + userName + '<h2>');
+askName();
+
+//  var userName = prompt('What is your name?');
+// document.write('<h2>' + 'Great to meet you ' + userName + '<h2>');
--- js/get-product.js
+++ js/get-product.js
@@ -1,16 +1,35 @@
 'use strict'
 
-var response = prompt('Do you like the color blue or red better?');
-var message;
+// var response = prompt('Do you like the color blue or red better?');
+// var message;
 
-if (response === 'blue') {
-message = 'blue is great!';
-}
-else if (response === 'red') {
-    message = 'red is great!';
-}
-else {
-    message = 'thats understandable';
+// if (response === 'blue') {
+// message = 'blue is great!';
+// }
+// else if (response === 'red') {
+//     message = 'red is great!';
+// }
+// else {
+//     message = 'thats a bold choice';
+// }
+
+// document.write('<h3>' + message + '</h3>');
+
+function getColors() {
+    var response = prompt('Is your favorite color blue or red?');
+    var message;
+
+    if (response === 'blue') {
+    message = 'Blue is great!';
+    }
+    else if (response === 'red') {
+        message = 'Red is great!';
+    }
+    else {
+        message = "That's a bold choice";
+    }
+
+    return document.write('<h3>' + message + '</h3>');
 }
 
-document.write('<h3>' + message + '</h3>');
+getColors();
